8a9d88a8af22f2bc64ed00957670ec1ee411578e
Cristian Calderón <cristian@iqdevpro.com>
2017-06-26 17:27:12 -0600

    Agregado diseño de pantalla principal

--- HackAtHomeClient/Resources/Resource.Designer.cs
+++ HackAtHomeClient/Resources/Resource.Designer.cs
@@ -60,6 +60,40 @@ namespace HackAtHomeClient
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050004
+			public const int buttonValidate = 2131034116;
+			
+			// aapt resource value: 0x7f050001
+			public const int editText1 = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int editText2 = 2131034115;
+			
+			// aapt resource value: 0x7f050005
+			public const int linearLayout1 = 2131034117;
+			
+			// aapt resource value: 0x7f050006
+			public const int linearLayout2 = 2131034118;
+			
+			// aapt resource value: 0x7f050000
+			public const int textViewEmail = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textViewPassword = 2131034114;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
