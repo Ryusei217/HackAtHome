c7fca757a92090cc9ce0830a2f5b50887a9a8d80
Cristian Calderón <cristian@iqdevpro.com>
2017-06-26 17:48:12 -0600

    Implementada Autenticacion

--- HackAtHomeClient/MainActivity.cs
+++ HackAtHomeClient/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using HackAtHome.SAL;
+using HackAtHome.Entities;
 
 namespace HackAtHomeClient
 {
@@ -13,6 +15,29 @@ namespace HackAtHomeClient
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            var validateButton = FindViewById<Button>(Resource.Id.buttonValidate);
+            validateButton.Click += (s, ev) =>
+            {
+                Validate();
+            };
+        }
+
+        private async void Validate()
+        {
+            ServiceClient ServiceClient = new ServiceClient();
+            var emailEditText = FindViewById<EditText>(Resource.Id.editTextEmail);
+            var passwordEditText = FindViewById<EditText>(Resource.Id.editTextPassword);
+
+            ResultInfo Result = await ServiceClient.AutenticateAsync(emailEditText.Text, passwordEditText.Text);
+
+            Android.App.AlertDialog.Builder Builder = new AlertDialog.Builder(this);
+            AlertDialog Alert = Builder.Create();
+            Alert.SetTitle("Resultado de la Verificación");
+            Alert.SetIcon(Resource.Drawable.Icon);
+            Alert.SetMessage($"{Result.Status}\n{Result.FullName}\n{Result.Token}");
+            Alert.SetButton("Ok", (s, ev) => { });
+            Alert.Show();
         }
     }
 }
